e9e6e477d57896b00ebd8eff8f999a243746cca6
faisalalik9 <66014401+faisalalik9@users.noreply.github.com>
2020-11-26 15:14:18 +0530

    Add files via upload

---
+++ firebase.init.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyDl7O47jcQ53Y6z3KqiR8-sJIFb1EW72tU",
+  authDomain: "chatty-app-e8c13.firebaseapp.com",
+  databaseURL: "https://chatty-app-e8c13.firebaseio.com",
+  projectId: "chatty-app-e8c13",
+  storageBucket: "chatty-app-e8c13.appspot.com",
+  messagingSenderId: "979645707949",
+  appId: "1:979645707949:web:6004e0844acce76234f824"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
